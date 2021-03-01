082e47f55523f1a6b4cf25f35f40a5216f1d5283
LeoneLal <45849538+LeoneLal@users.noreply.github.com>
2021-03-01 15:48:32 +0100

    TP1 partie 2
     Ajouter des nouveaux posts au fichier JSON préexistant sans supprimer les données précédentes

--- TPNotes/app.js
+++ TPNotes/app.js
@@ -3,18 +3,24 @@ const post = require('./post.js')
 const cmd = process.argv;
 
 const commandes = {
-    add: "Commande add",
-    remove: "Commande remove",
-    list: "Commande list",
-    read: "Commande read"
+    add: "Commande : add",
+    remove: "Commande : remove",
+    list: "Commande : list",
+    read: "Commande : read"
 }
 
 console.log(`${commandes[cmd[2]] || "unknow command"}`)
 
-if(cmd[2] == "add"){
+if(cmd[2] === "add"){
     post.addPost(cmd[3], cmd[4]) 
 }
-else if (cmd[2] == "list") {
-    post.listPosts() 
+else if (cmd[2] === "list") {
+    const tab = post.listPosts();
+    console.log(`Printing ${tab.length} posts `)
+    tab.forEach(post => {
+        console.log('--');
+        console.log(`Title: ${post.title}`);
+        console.log(`Body: ${post.body}`);
+    });
 }
 
--- TPNotes/post.js
+++ TPNotes/post.js
@@ -4,36 +4,53 @@ function addPost(titre, corp) {
     if(titre == undefined){
         console.log("Il manque le titre et le body");
         return "Il manque le titre";
-    }if(corp == undefined){
+    } 
+    if(corp == undefined){
         console.log("Il manque le body");
         return "Il manque le body";
-    } else {
+    } 
+    else {
+        const tab = listPosts();
+        let alreadyExist = false
         let data = {
             title: titre,
             body: corp
         };
-        fs.appendFile('posts.json', JSON.stringify([data]), 'utf8', (err, data) =>{
-            if(err) throw err;
-            console.log("Succesfully added");
+        tab.forEach(post => {
+           if (post.title === data.title){
+               console.log("Ce titre existe déjà");
+               alreadyExist = true;
+           }
         });
+        if ( alreadyExist === false){
+            tab.push(data)        
+            fs.writeFile('posts.json', JSON.stringify(tab), 'utf8', (err) =>{
+                if(err) throw err;
+                console.log("Succesfully added");
+            });
+        };
     };
 }
 
-function listPosts() {
-    let final = ""
-    fs.readFile('./posts.json', 'utf8', (err, data) =>{
-        if(err) throw err;
-        const res = JSON.parse(data)
-        final += "Printing " + res.length + " posts \n"
-        for(let post of res){
-            let title = "Title : " + post.title
-            let body = "Body : " + post.body
-            final += "-- \n" + title + "\n" + body + "\n"
-        }
+// function listPosts() {
+//     let final = ""
+//     fs.readFile('./posts.json', 'utf8', (err, data) =>{
+//         if(err) throw err;
+//         const res = JSON.parse(data)
+//         final += "Printing " + res.length + " posts \n"
+//         for(let post of res){
+//             let title = "Title : " + post.title
+//             let body = "Body : " + post.body
+//             final += "-- \n" + title + "\n" + body + "\n"
+//         }
 
-        console.log(final)
-        return final
-    })
+//         console.log(final)
+//         return final
+//     })
+// }
+
+function listPosts() {
+    return  JSON.parse(fs.readFileSync('./posts.json', 'utf8'));
 }
 
 module.exports = {addPost, listPosts}
